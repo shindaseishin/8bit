b7774852ed6c17fa1c448da842db8012ed128d5c
Andrew Trebble <andrew.trebble@gmail.com>
2018-07-07 18:31:30 -0400

    Add help message

--- help.py
+++ help.py
@@ -10,6 +10,7 @@ class Help(Component):
     def display(self):
         self._window.addstr(2, 2, "[q] Quit\t[p] Pause\t[h] Halt\t[r] Reset")
         self._window.addstr(3, 2, "[a] Faster\t[z] Slower\t[o] Pulse")
+        self._window.addstr(4, 2, "[\u2191] Scroll Up\t[\u2193] Scroll Down")
         self._window.refresh()
 
 
